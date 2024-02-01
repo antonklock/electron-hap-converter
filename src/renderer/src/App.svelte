4c9201a6125066341ba64b40fde959b84ab8c1cd
Anton Klock <klock.anton@gmail.com>
2024-02-01 13:35:19 +0100

    updating frontend

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,29 +1,17 @@
 <script lang="ts">
-  import Versions from './components/Versions.svelte'
   import electronLogo from './assets/electron.svg'
   import './tailwind.css'
 
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 </script>
 
-<p class="text-red-400 text-4xl">Hello world!</p>
-<button class="btn btn-primary my-2">Button</button>
-<img alt="logo" class="logo" src={electronLogo} />
-<div class="creator">Powered by electron-vite</div>
-<div class="text">
-  Build an Electron app with
-  <span class="svelte">Svelte</span>
-  and
-  <span class="ts">TypeScript</span>
+<h1 class="text-4xl font-black mb-2">Convert ProRes to HAP</h1>
+
+<div class="mb-10">
+  <input type="text" class="input" placeholder="Input folder" />
 </div>
-<p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
-<div class="actions">
-  <div class="action">
-    <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
-  </div>
-  <div class="action">
-    <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions a11y-missing-attribute-->
-    <a target="_blank" rel="noreferrer" on:click={ipcHandle}>Send IPC</a>
-  </div>
-</div>
-<Versions />
+
+<img alt="logo" class="w-32" src={electronLogo} />
+
+<div class="mt-10"></div>
+<input type="text" class="input" placeholder="Output folder" />
